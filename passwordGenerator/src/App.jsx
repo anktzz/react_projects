29415068fd6985da40508b34baaf4b841115507c
anktzz <ankitgupta271105@gmail.com>
2025-05-10 13:09:03 +0530

    setup enviornment & parameters

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -3,9 +3,14 @@ import { useState } from 'react'
 
 function App() {
 
+  const [length, setLength] = useState(8)
+  const [numAllowed, setNumAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
   return (
     <>
-
+      <h1 className='text-4xl text-center text-white'>Password</h1>
     </>
   )
 }
